b90c91f4e718721f16b7b9ac7da4ff1a521175de
Pete1701 <pete.pertongonas@pirron.one>
2020-03-12 00:39:00 +0100

    dark-mode MVP

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -4,12 +4,14 @@ import { useLocalStorage } from './useLocalStorage';
 export const useDarkMode = () => {
     const [value, setValue] = useLocalStorage('dark-mode', false);
     useEffect(() => {
-        if(useLocalStorage.value === true) {
+        // const body = document.querySelector('body')
+        // return value ? body.classList.add('dark-mode') : body.classList.remove('dark-mode')
+        if(value === true) {
             document.body.classList.add('dark-mode');
         } else {
             document.body.classList.remove('dark-mode');
         }        
-      });
+      }, [value]);
 
       return [value, setValue];
 }
